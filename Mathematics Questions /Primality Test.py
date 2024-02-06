f3dfc8c369ec220990e2e0fabd28be210b9e08ad
HarshSonaiya <124482837+HarshSonaiya@users.noreply.github.com>
2024-02-06 16:01:34 +0530

    Update Primality Test.py

--- Mathematics Questions /Primality Test.py
+++ Mathematics Questions /Primality Test.py
@@ -1,10 +1,9 @@
-// Prime numbers are those that are divisible only by 1 and themselves.
-// Given a number N, check if it is prime or not.
+'''
+Prime numbers are those that are divisible only by 1 and themselves.
+Given a number N, check if it is prime or not.
 
-// Python Code:
-
-// Approach 1: Brute Force Method (Time Complexity T(n) = O(n))
-
+Approach 1: Brute Force Method (Time Complexity T(n) = O(n))
+'''
 def isPrime(n):
     if n == 1:
         return False
@@ -12,12 +11,13 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
-
-// Approach 2: Time Complexity T(n) = O(sqrt(n))
-// Divisors always occur in pairs. For example, check if 10 is prime or not.
-// Factors of 10 are 1, 2, 5, 10 --> 1 x 10 = 10, 2 x 5 = 10, 5 x 2 = 10, 10 x 1 = 10
-// So if (x, y) is a pair of factors like (2, 5) and x is less than or equal to y,
-// then the following is always true for (x, y): x * x < n (because x is less than y)
+'''
+ Approach 2: Time Complexity T(n) = O(sqrt(n))
+ Divisors always occur in pairs. For example, check if 10 is prime or not.
+ Factors of 10 are 1, 2, 5, 10 --> 1 x 10 = 10, 2 x 5 = 10, 5 x 2 = 10, 10 x 1 = 10
+ So if (x, y) is a pair of factors like (2, 5) and x is less than or equal to y,
+ then the following is always true for (x, y): x * x < n (because x is less than y)
+'''
 
 def isPrime(n):
     if n == 1:
@@ -28,13 +28,13 @@ def isPrime(n):
             return False
         i += 1
     return True
-
-// Approach 3: Optimized Approach T(n) = O(sqrt(n))
-// Every prime number (P) can be expressed in the form "6K + 1" or "6K - 1".
-// Thus, check if P is prime by dividing from 2 to sqrt(n) but skipping multiples of 6
-// because if all other numbers except multiples of 6 cannot divide the number P,
-// it means it can be expressed in the form "6K + 1" or "6K - 1". Hence, it is a prime number.
-
+'''
+ Approach 3: Optimized Approach T(n) = O(sqrt(n))
+Every prime number (P) can be expressed in the form "6K + 1" or "6K - 1".
+ Thus, check if P is prime by dividing from 2 to sqrt(n) but skipping multiples of 6
+ because if all other numbers except multiples of 6 cannot divide the number P,
+ it means it can be expressed in the form "6K + 1" or "6K - 1". Hence, it is a prime number.
+'''
 import math
 
 def isPrime(N):
